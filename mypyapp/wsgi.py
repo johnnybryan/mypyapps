a85d564f099544cf8f417a008f2fac59b479547f
Johnny Bryan <johnnybryan21@gmail.com>
2022-02-13 09:24:41 -0500

    initial commit

---
+++ mypyapp/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for mypyapp project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mypyapp.settings')
+
+application = get_wsgi_application()
